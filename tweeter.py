4ba69c7cd78d60f507233a822cc144fef2bd1bf9
olympicmew <olympicmew@gmail.com>
2019-01-07 23:27:29 +0000

    Add dry run support to tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -8,6 +8,7 @@ DBPATH = os.environ['DBPATH']
 
 argparser = argparse.ArgumentParser()
 argparser.add_argument('type')
+argparser.add_argument('--dry-run', action='store_true')
 args = argparser.parse_args()
 
 logging.basicConfig(filename='bot.log', level=logging.INFO,
@@ -17,8 +18,14 @@ db = sqlite3.connect(DBPATH)
 logging.info('Database connected')
 
 if args.type == 'followers-daily':
-	status = lttbot.followers_update(db, 'daily')
-	logging.info(status)
+	status = lttbot.followers_update(db, 'daily', args.dry_run)
+	if args.dry_run:
+		print(status)
+	else:
+		logging.info(status)
 elif args.type == 'followers-weekly':
-	status = lttbot.follwers_update(db, 'weekly')
-	logging.info(status)
+	status = lttbot.follwers_update(db, 'weekly', args.dry_run)
+	if args.dry_run:
+		print(status)
+	else:
+		logging.info(status)
