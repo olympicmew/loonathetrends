1fefd20c380762e78d17cad4b295b76a34781231
olympicmew <olympicmew@gmail.com>
2019-01-12 03:42:27 +0100

    Convert tabs to spaces

--- loonathetrends/utils.py
+++ loonathetrends/utils.py
@@ -2,15 +2,15 @@ import ephem
 import pandas as pd
 
 def get_moon_phase(date):
-	date = ephem.Date(date.datetime)
-	nnm = ephem.next_new_moon(date)
-	pnm = ephem.previous_new_moon(date)
-	return (date-pnm) / (nnm-pnm)
+    date = ephem.Date(date.datetime)
+    nnm = ephem.next_new_moon(date)
+    pnm = ephem.previous_new_moon(date)
+    return (date-pnm) / (nnm-pnm)
 
 def get_moon_emoji(phase):
-	emojis = '🌑🌒🌓🌔🌕🌖🌗🌘'
-	idx = round(phase*8) % 8
-	return emojis[idx]
+    emojis = '🌑🌒🌓🌔🌕🌖🌗🌘'
+    idx = round(phase*8) % 8
+    return emojis[idx]
 
 def get_video_title_lookup(db):
     df = pd.read_sql('select video_id, title from videos',
