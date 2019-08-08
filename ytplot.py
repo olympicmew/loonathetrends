0ed504616551517f79de5bb7c680417b01f96937
olympicmew <olympicmew@gmail.com>
2019-08-08 20:08:51 +0900

    Add utility script to generate graphs

---
+++ ytplot.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+import sqlite3
+import sys
+import loonathetrends.bot.plots as lttplots
+
+DBPATH = os.getenv("DBPATH")
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument("videoid")
+argparser.add_argument("--metric", "-m", default="views")
+argparser.add_argument("--timeframe", "-t", default="short")
+args = argparser.parse_args()
+
+db = sqlite3.connect(DBPATH)
+
+plot = lttplots.youtube(db, args.videoid, metric=args.metric, timeframe=args.timeframe)
+sys.stdout.buffer.write(plot)
+
---
+++ ytplot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DBPATH=$HOME/loonathetrends/loonathetrends.db
+export IMGUR_CLIENTID="***REMOVED***"
+cd $HOME/loonathetrends
+source venv/bin/activate
+python ytplot.py $@ | \
+curl -L -XPOST "https://api.imgur.com/3/image" \
+     -H "Authorization: Client-ID $IMGUR_CLIENTID" -F "image=@-" -s | \
+python -c "import sys, json; print(json.load(sys.stdin).get('data').get('link'))"
